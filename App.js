3683519b6bd0168858e1dff1e7b22a9c6b843394
Nayandin77 <nayandindennis@yahoo.com>
2020-03-11 00:03:24 -0400

    finished basic app

--- App.js
+++ App.js
@@ -1,19 +1,45 @@
 import React, { useState } from 'react';
-import { View, Text, StyleSheet, FlatList } from 'react-native';
+import { View, Text, StyleSheet, FlatList, Alert } from 'react-native';
+import uuid4 from 'uuid4';
+
 import Header from './components/Header';
+import ListItem from './components/ListItem';
+import AddItem from './components/AddItem';
 
 const App = () => {
   const [items, setItems] = useState([
-    { id: 1, text: 'Milk' },
-    { id: 2, text: 'Eggs' },
-    { id: 3, text: 'Bread' },
-    { id: 4, text: 'Chocolate' },
+    { id: uuid4(), text: 'Milk' },
+    { id: uuid4(), text: 'Eggs' },
+    { id: uuid4(), text: 'Bread' },
+    { id: uuid4(), text: 'Chocolate' },
   ]);
+
+
+  const deleteItem = (id) => {
+    setItems(previousItems => {
+      return previousItems.filter(item => item.id != id); // returns all the ids but not the id being deleted
+    });
+  };
+
+  const addItem = (text) => {
+    if(!text) {
+      Alert.alert('Error', 'Please enter an item name', {text:'Ok'});
+    } else {
+      setItems(previousItems => {
+        return [{id: uuid4(), text}, ...previousItems];
+      });
+    }
+  };
 
   return (
     <View style={styles.container}>
       <Header title='Shopping List' />
-      <FlatList />
+      <AddItem addItem={addItem} />
+      <FlatList 
+        data={items}
+        renderItem={({item}) => <ListItem item={item} 
+        deleteItem={deleteItem} />}
+      />
     </View>
   )
 }
--- components/Header.js
+++ components/Header.js
@@ -17,7 +17,7 @@ const styles = StyleSheet.create({
   header: {
     height: 60,
     padding: 15,
-    backgroundColor: '#001'
+    backgroundColor: '#003'
   },
   text: {
       color: '#fff',
